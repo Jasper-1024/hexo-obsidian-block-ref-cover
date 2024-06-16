f816abe5854f9ab21d956d10f7e346200d90c610
Jasper Hale <ljy087621@gmail.com>
2024-06-16 11:43:08 +0000

    Update hexo-filter-titlebased-link to replace link IDs and references

--- index.js
+++ index.js
@@ -1,39 +1,74 @@
-'use strict';
-
-const log = require('hexo-log').default({
+const log = require('hexo-log')({
   debug: false,
   silent: false
 });
 
 const cachedPost = {};
 let lastPost;
-hexo.extend.filter.register('post_permalink', function(data){
+
+function cachePost(data) {
   lastPost = data;
   return data;
-}, 1);
+}
 
-hexo.extend.filter.register('post_permalink', function(permalink) {
+function cachePermalink(permalink) {
   if (lastPost) {
     const fileName = lastPost.source.match(/[^/]*$/)[0].replace(/\.md$/, '');
     cachedPost[fileName] = permalink;
   }
   return permalink;
-}, 25);
+}
 
-hexo.extend.filter.register("before_post_render", (post) => {
-  const re = /\[\[([^\*"\\/<>:?\[\]\|]+)\|?([^\*"\\/<>:|?\[\]]*)?\]\]/g;
-  post.content = post.content.replace(re, function(match, p1,p2) {
-    const fileName = decodeURI(p1);
-    if (cachedPost[fileName]) {
-      log.info("hexo-filter-titlebased-link: Replace -",fileName);
-      return p2?`<a href="/${cachedPost[fileName]}">${decodeURI(p2)}</a>`:`<a href="/${cachedPost[fileName]}">${fileName}</a>`;
+hexo.extend.filter.register('post_permalink', cachePost, 1);
+hexo.extend.filter.register('post_permalink', cachePermalink, 25);
+
+function replaceLinkId(data) {
+  const re = /\^(\w+)$/gm;
+  data.content = data.content.replace(re, (match, marker) => {
+    const uniqueId = "^" + marker;
+    log.info("hexo-filter-titlebased-link: Replace ID", marker);
+    return `<span id="${uniqueId}" style="font-size: smaller; color: gray; vertical-align: top; opacity: 0.75;">${match}</span>`;
+  });
+    
+}
+
+function replaceRef(post) {
+  const re = /\[\[(?:(?<file>[^\n#\|\]]+)?(?:#(?<title>[^\n\|\]]+))?)(?:\|(?<text>[^\n\]]+))?\]\]/g;
+
+  post.content = post.content.replace(re, function (match, p1, p2, p3) {
+    let fileName;
+    let linkText;
+    let title;
+
+    try {
+      fileName = p1 ? decodeURI(p1) : post.source.match(/[^/]*$/)[0].replace(/\.md$/, '');
+      title = p2 ? decodeURI(p2) : null; // p2 is the title
+      linkText = p3 ? decodeURI(p3) : fileName + (title ? `#${title}` : ''); // p3 is the link text
+    } catch (e) {
+      log.error("hexo-filter-titlebased-link: Invalid URI -", p1);
+      return `<a href="#">Invalid link</a>`; // Return a default link if URI is invalid
     }
+
+    if (cachedPost[fileName]) {
+      log.info("hexo-filter-titlebased-link: Replace -", fileName);
+      let href = `/${cachedPost[fileName]}`;
+      if (title) {
+        href += `#${title}`;
+      }
+      return `<a href="${href}">${linkText}</a>`;
+    } else {
+      log.warn("hexo-filter-titlebased-link: No cached permalink for -", fileName);
+    }
+
     return match;
   });
   return post;
-});
+}
 
+hexo.extend.filter.register('before_post_render', replaceLinkId);
+hexo.extend.filter.register("before_post_render", replaceRef);
 
-
-
-
+module.exports = {
+  replaceLinkId,
+  replaceRef,
+};
